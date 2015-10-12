4db05da7eb706984cf0ec6d7ce5da9ba190c131f
Mark Daniels <mark_daniels1982@hotmail.com>
2015-10-12 14:35:23 -0700

    add schema.sql,flaskr.py

---
+++ flaskr.py
@@ -0,0 +1,36 @@
+#imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+#init app
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR SETTINGS', silent=True)
+
+# connect db
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+#init db
+if __name__== '__main__':
+    app.run()
